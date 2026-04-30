5101c1c022dba8838df09921f23b521e9068b47b
Josue <jr5766236@gmail.com>
2026-04-30 13:30:30 -0600

    Operators

---
+++ src/main/java/daysofCode/Solution.java
@@ -0,0 +1,38 @@
+package daysofCode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Result {
+     public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+         double tip = meal_cost * tip_percent / 100.0;
+
+         double tax = meal_cost * tax_percent / 100.0;
+
+         double total_cost = meal_cost + tip + tax;
+
+         int final_cost = (int) Math.round(total_cost);
+
+
+         System.out.println(final_cost);
+     }
+
+}
+
+public class Solution{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+
+}
