f45c526d179478373ad9a7357244b47681905006
Josue <jr5766236@gmail.com>
2026-05-01 14:30:40 -0600

    Conditional

---
+++ src/main/java/daysofCode/Conditional.java
@@ -0,0 +1,35 @@
+package daysofCode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+
+        if (N % 2 != 0) {
+            // Si N es impar (odd)
+            System.out.println("Weird");
+        } else {
+
+            if (N >= 2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else if (N > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+
+        bufferedReader.close();
+    }
+
+
+
+
+}
