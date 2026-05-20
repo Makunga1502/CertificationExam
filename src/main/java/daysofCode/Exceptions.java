be789ed3e4935756276d954839a1fcb0ffcffe03
Josue <jr5766236@gmail.com>
2026-05-19 19:40:15 -0600

    ejercicios del reto de 30 días

---
+++ src/main/java/daysofCode/Exceptions.java
@@ -0,0 +1,17 @@
+package daysofCode;
+
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String string = in.next();
+        in.close();
+        try {
+            Integer integer = Integer.parseInt(string);
+            System.out.println(integer);
+        } catch (NumberFormatException numberFormatException) {
+            System.out.println("Bad String");
+        }
+    }
+}
---
+++ src/main/java/daysofCode/Recursion3.java
@@ -0,0 +1,23 @@
+package daysofCode;
+
+import java.util.Scanner;
+
+public class Recursion3 {
+        static int factorial(int n) {
+            if (n <= 1) {
+                return 1;
+            }
+            else {
+                return n * factorial(n - 1);
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int result = factorial(n);
+            System.out.println(result);
+            in.close();
+        }
+}
+
