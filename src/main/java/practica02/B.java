ce2507e0aba3bdfadb8f5b43819a08f88190a8ac
Josue <jr5766236@gmail.com>
2026-05-21 08:27:01 -0600

    ejercicios test

---
+++ src/main/java/practica02/B.java
@@ -0,0 +1,4 @@
+package practica02;
+
+public class B {
+}
---
+++ src/main/java/practica03/Hola.java
@@ -0,0 +1,8 @@
+package practica03;
+
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+
+    }
+}
